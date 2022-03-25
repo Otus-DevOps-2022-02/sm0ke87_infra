feeab90dd1cb626a841df4c2a549e34926aa472c
sm0ke87 <s.alimov87@yahoo.com>
2022-03-25 10:20:00 +0300

    Fastfix #6

--- install_ruby.sh
+++ install_ruby.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
-sudo apt-get install -y ruby-full ruby-bundler build-essential
+sudo apt-get update -y
+sudo apt-get install apt-transport-https ca-certificates -y
+sudo apt-get update -y
+sudo apt-get upgrade -y
+sudo apt-get install ruby-full -y
+sudo apt-get install ruby-bundler -y
+sudo apt-get install build-essential -y
 ruby -v
 bundler -v
