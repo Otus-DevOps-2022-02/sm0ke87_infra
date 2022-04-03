d82b46ee2c2a39dab01e595d2c361b3c31617840
sm0ke87 <s.alimov87@yahoo.com>
2022-04-03 23:11:46 +0300

    adding 9.1 and 9.2

---
+++ packer/scripts/install_mongodb.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+wget -qO - https://www.mongodb.org/static/pgp/server-4.2.asc | sudo apt-key add -
+echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/4.2 multiverse" | tee /etc/apt/sources.list.d/mongodb-org-4.2.list
+apt-get update -y
+sleep 100
+apt-get install -y mongodb-org
+sleep 5
+systemctl start mongod
+systemctl enable mongod
---
+++ packer/scripts/install_ruby.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+apt-get install apt-transport-https ca-certificates -y
+sleep 5
+apt-get update -y
+sleep 100
+apt-get install ruby-full -y
+sleep 5
+apt-get install ruby-bundler -y
+sleep 5
+apt-get install build-essential -y
+sleep 5
+ruby -v
+bundler -v
