ee31b43dac727fb7550984746a5e205214bec83f
sm0ke87 <s.alimov87@yahoo.com>
2022-04-12 16:59:10 +0300

    add stage and prod

--- terraform/modules/db/main.tf
+++ terraform/modules/db/main.tf
@@ -9,9 +9,11 @@ resource "yandex_compute_instance" "db" {
     }
   }
   network_interface {
-    subnet_id = yandex_vpc_subnet.app-subnet.id
+    subnet_id = var.subnet_id
     nat = true
   }
+
+
 
   metadata = {
     ssh-keys = "ubuntu:${file(var.public_key_path)}"
